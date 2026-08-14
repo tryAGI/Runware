dec11bebd209092ba8222c9c98c2dbb972114973
Konstantin S. <havendv@gmail.com>
2026-08-14 19:42:40 +0400

    feat: Updated OpenAPI spec (#17)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Runware/Generated/Runware.RunwareClient.g.cs
+++ src/libs/Runware/Generated/Runware.RunwareClient.g.cs
@@ -39,10 +39,17 @@ namespace Runware
 
         /// <inheritdoc/>
         public global::Runware.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Runware.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Runware.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
